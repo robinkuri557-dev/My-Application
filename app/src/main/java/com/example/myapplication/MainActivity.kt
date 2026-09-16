4c3f164d3e9e5579dcd222ad9a3ba16cc64af1cd
robinkuri557-dev <robinkuri557@gmail.com>
2026-09-17 02:12:12 +0300

    Add Button and TextView to layout, handle button click to update TextView text

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val textView = findViewById<TextView>(R.id.textView)
+        val button1 = findViewById<Button>(R.id.button1)
+        button1.setOnClickListener {
+            textView.text = "Button was clicked!"
+        }
+
     }
 }
